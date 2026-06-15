5b5808c395860bf959e34c06288f480420ca22dd
Kovács Patrik <65179610+Patrik2020@users.noreply.github.com>
2026-06-15 15:14:11 +0200

    Add files via upload

--- js/global-head.js
+++ js/global-head.js
@@ -13,11 +13,15 @@ const projectRoot = rootParts.length ? `/${rootParts.join("/")}` : "";
 const basePath = `${projectRoot}/favicon`;
 
 const headContent = `
+<link rel="icon" type="image/png" href="${basePath}/favicon-16x16.png" sizes="16x16" />
+<link rel="icon" type="image/png" href="${basePath}/favicon-32x32.png" sizes="32x32" />
 <link rel="icon" type="image/png" href="${basePath}/favicon-96x96.png" sizes="96x96" />
 <link rel="icon" type="image/svg+xml" href="${basePath}/favicon.svg" />
 <link rel="shortcut icon" href="${basePath}/favicon.ico" />
 <link rel="apple-touch-icon" sizes="180x180" href="${basePath}/apple-touch-icon.png" />
-<meta name="apple-mobile-web-app-title" content="Kalkulátor Bázis" />
+<meta name="application-name" content="KalkulátorBázis" />
+<meta name="apple-mobile-web-app-title" content="KalkulátorBázis" />
+<meta name="theme-color" content="#0b66f2" />
 <link rel="manifest" href="${basePath}/site.webmanifest" />
 `;
 
